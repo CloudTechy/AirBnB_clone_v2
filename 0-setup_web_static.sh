e2200e6a90f7cc3f0db19f592f22bd27b1e51e56
Onyekelu Chukwuebuka <conyekelu@yahoo.com>
2024-05-03 15:57:33 -0700

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,7 +7,16 @@
 if ! command nginx -v  &> /dev/null;  then
 	sudo apt-get -y install nginx
 fi
+#setup init files
+echo 'Holberton School' > /var/www/html/index.html
+echo "Ceci n'est pas une page" > /var/www/error/404.html
 
+sed -i '/server_name _/a location /redirect_me { rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlw\u4? permanent; }' /etc/nginx/sites-available/default
+sed -i '/server_name _/a error_page 404 /404.html; location = /404.html {root /var/www/error/; internal; }' /etc/nginx/sites-available/default
+
+sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+
+#sets up deployment for webstatic
 sudo mkdir -p /data/web_static/releases/test/
 
 sudo echo "<h1>  Nginx is Active </h1>" > /data/web_static/releases/test/index.html
