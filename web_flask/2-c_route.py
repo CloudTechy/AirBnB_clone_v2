f3e0cc6ccdb0505e4f18f3e3593bc016fbaf3998
Onyekelu Chukwuebuka <o.enje@rgu.ac.uk>
2024-05-17 13:03:06 +0100

    creates a /c/<text> route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Module that display “Hello HBNB! """
+
+from flask import Flask
+
+# instantiate app
+app = Flask(__name__)
+
+# create the index or home route
+
+
+@app.route('/')
+def index():
+    """A route controller for the index page """
+    return "Hello HBNB!"
+
+
+# create the /hbnb route
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """A route controller for the hbnb page """
+    return "HBNB"
+
+
+# creates a /c/<text> route
+@app.route('/c/<text>')
+def c(text):
+    content = text.replace("_", " ")
+    return (content)
+
+
+# run the development server
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
