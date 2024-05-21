c7c901be058cb3cf3a1826deb680ffc3ee832169
Onyekelu Chukwuebuka <onyekeluchukwuebuka@yahoo.ca>
2024-05-21 12:23:05 +0100

    places

---
+++ web_flask/9-states.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Modeule that starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all place objects in DBStorage.
+"""
+
+from models import storage
+from flask import Flask, render_template
+import sys
+import os
+
+# Get the absolute path to the root directory of your project
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(ROOT_DIR)
+
+
+app = Flask(__name__)
+
+
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays an HTML page with a list of all States.
+
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
